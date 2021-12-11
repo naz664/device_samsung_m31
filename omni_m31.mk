b396e72882680afd5c34d924f9660b6828699543
naz664 <nazimnavas145@gmail.com>
2021-12-11 18:33:06 +0530

    PBRP bringup

--- omni_m31.mk
+++ omni_m31.mk
@@ -1,8 +1,14 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Include any options that can't be included in BoardConfig.mk
 $(call inherit-product, device/samsung/m31/device.mk)
+
+# Inherit some common PitchBlack stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_m31
